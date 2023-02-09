84a0f96538ebfb307883dcea324b03d9de308551
ziedAllouen <uchihazied@gmail.com>
2023-02-09 23:24:28 +0100

    bv

---
+++ dist/users/users.controller.d.ts
@@ -0,0 +1,6 @@
+import { UsersService } from './users.service';
+export declare class UsersController {
+    private readonly service;
+    constructor(service: UsersService);
+    Add(): string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    constructor( private readonly service: UsersService){}
+
+    @Post()
+    Add(){
+        return 'add action' ;
+    }
+}
